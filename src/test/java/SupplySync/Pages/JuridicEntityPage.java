a0ad309a5801341f7d8e775e563d04fdfa2ef8da
Aizhan Nazirbaeva <aizhannazirbaeva@Aizhans-Air.attlocal.net>
2024-01-10 23:18:03 -0600

    added JuridicEntityPage

--- src/test/java/SupplySync/Pages/JuridicEntityPage.java
+++ src/test/java/SupplySync/Pages/JuridicEntityPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class JuridicEntityFunctionality {
+public class JuridicEntityPage {
     public WebDriver driver;
-    public JuridicEntityFunctionality(WebDriver driver){
+    public JuridicEntityPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
