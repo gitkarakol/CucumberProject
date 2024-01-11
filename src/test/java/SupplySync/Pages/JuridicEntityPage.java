99a2f6d4d351f4ffec3b8c7b6eec1e5424f1e393
Aizhan Nazirbaeva <aizhannazirbaeva@Aizhans-Air.attlocal.net>
2024-01-10 23:16:42 -0600

    added JuridicEntityPage

---
+++ src/test/java/SupplySync/Pages/JuridicEntityPage.java
@@ -0,0 +1,43 @@
+package SupplySync.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class JuridicEntityFunctionality {
+    public WebDriver driver;
+    public JuridicEntityFunctionality(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(xpath = "//span[text()='Juridic Entity']")
+    public WebElement juridicEntity;
+
+    @FindBy(xpath = "//div[@class='sc-gJbFto jUsODE']//button ")
+    public WebElement createLegalEntityButton ;
+
+    @FindBy(id = "mui-component-select-companyId")
+    public WebElement companiesField ;
+
+    @FindBy(xpath = "//div[@id='menu-companyId']//li)[1] ")
+    public WebElement chooseCompany ;
+
+    @FindBy(id = "mui-component-select-branchId ")
+    public WebElement branchField ;
+
+    @FindBy(xpath = "//div[@id='menu-branchId']//li[text()='Blanda, Simonis and Kris'] ")
+    public WebElement chooseBranch ;
+
+    @FindBy(id = "mui-2")
+    public WebElement legalEntityNameField ;
+
+    @FindBy(xpath = "//div[@class='sc-jnrVZQ ieeBTg']/button[text()='Create']")
+    public WebElement createButton ;
+
+
+
+
+
+
+}
